5bc47bb82cd854678f36d2a0c25d5a44b4653557
SANELE-221053955 <221053955@student.uj.ac.za>
2024-10-14 17:14:42 +0200

    updated the user home page,added forecast, and chatBot

--- LinkedCodes/app/(userTabs)/home.jsx
+++ LinkedCodes/app/(userTabs)/home.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, FlatList, ActivityIndicator, Linking } from 'react-native';
+import { View, Text, Image, FlatList, ActivityIndicator, Linking,TouchableOpacity } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
@@ -6,8 +6,10 @@ import axios from 'axios';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CurrentDay from "../components/weather-API/CurrentDay";
 import { News } from '../components/News-API/News';
+import { router } from 'expo-router';
 
-const Home = () => {
+
+const Home = ({route}) => {
 
   const [news, setNews] = useState([]);
   const [error, setErrors] = useState(null);
@@ -45,14 +47,14 @@ const Home = () => {
   const renderItem = ({ item }) => (
     <SafeAreaView style={{padding:1, backgroundColor:'#EAF1FF'}}>
  
-        <View style={{padding:10,borderBottomWidth: 1,borderBottomColor:'black',backgroundColor:'#fff',
-          borderRadius:5, marginVertical:-8, }}>
+      <View style={{padding:10,borderBottomWidth: 1,borderBottomColor:'black',backgroundColor:'#EAF1FF',
+          borderRadius:5, }}>
 
           <Text style={{fontSize: 22,fontWeight: 'bold', marginBottom:10 }}>
             {item.title}
           </Text>
           {item.urlToImage && <Image source={{ uri: item.urlToImage }}
-                  style={{width:'100%', height:200, marginBottom:10,borderRadius:5}} />}
+                  style={{width:'100%', height:200, marginBottom:10,borderRadius:9}} />}
 
           <Text style={{fontSize: 16,color: 'black',fontStyle: 'italic', fontWeight:'bold'}}>
             By {item.author || 'Unknown'}
@@ -68,30 +70,14 @@ const Home = () => {
                 onPress={() => Linking.openURL(item.url)}>
               Click to read more....
           </Text>
-       </View>
+      </View>
     </SafeAreaView>
   );
 
   return (
-    <View style={{marginHorizontal:4,padding:2}}>
-      {/* <Pressable
-        style={{
-          padding: 10,
-          borderRadius: 10,
-        }}
-        onPress={() => signOut(auth)}
-      >
-        <Text
-          style={{
-            color: "blue",
-            fontSize: 16,
-          }}
-        >
-          Logou
-        </Text>
-      </Pressable> */}
+    <View style={{marginBottom:5,padding:0,backgroundColor:'#EAF1FF' }}>
 
-      <View style={{}}>
+      <View style={{height:'18%'}}>
         <CurrentDay/>
       </View>
       <View style={{height:'83%'}}>
@@ -101,6 +87,14 @@ const Home = () => {
             keyExtractor={item => item.url}
           />
       </View>
+      <TouchableOpacity style={{ position: 'absolute',bottom: 20, right: 20, backgroundColor: '#202A44',
+             borderRadius: 25,  
+             padding: 10,
+             elevation: 5,
+            }}
+              >
+        <Text style={{fontSize:40,color:'white'}}> c </Text>
+      </TouchableOpacity>
     </View>
   );
 };

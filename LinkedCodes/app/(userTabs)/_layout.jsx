1c8432b1350b39bdcddcd2cb2c46e6b1ee5c09c9
Skyler <skylersur0811@gmail.com>
2024-10-23 19:29:46 +0200

    complete @Sanele-525 updates requests

--- LinkedCodes/app/(userTabs)/_layout.jsx
+++ LinkedCodes/app/(userTabs)/_layout.jsx
@@ -1,6 +1,6 @@
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
-
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 const Layout = () => {
   return (
     <Tabs initialRouteName="home"
@@ -18,7 +18,8 @@ const Layout = () => {
         name="home"
         options={{
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" color={color} size={size} />
+            // <Ionicons name="home-outline" color={color} size={size} />
+            <MaterialIcons name="home" size={30} color={color} />
           ),
         }}
       />
@@ -26,12 +27,18 @@ const Layout = () => {
         name="reporting"
         options={{
           tabBarIcon: ({ color }) => (
-            <Ionicons name="clipboard-outline" color={color} size={30} />
+          <MaterialIcons name="add-circle-outline" size={30} color={color} />
+            // <Ionicons name="clipboard-outline" color={color} size={28} />
           ),
         }}
-      />
+        />
       <Tabs.Screen
         name="traffic"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="traffic" size={30} color={color} />
+          ),
+        }}
       />
     </Tabs>
   );
--- LinkedCodes/app/(userTabs)/home/_layout.jsx
+++ LinkedCodes/app/(userTabs)/home/_layout.jsx
@@ -1,13 +1,37 @@
-import { StatusBar } from 'expo-status-bar'
-import { Stack } from 'expo-router'
+import { StatusBar } from "expo-status-bar";
+import { Stack, Tabs } from "expo-router";
+import { View } from "react-native";
 
 const Layout = () => {
-    return (
-        <>
-            <StatusBar style="dark" />
-            <Stack screenOptions={{ headerShown: false }} />
-        </>
-    )
-}
+  return (
+    <>
+      <Tabs.Screen
+        options={{
+          tabBarVisibilityAnimationConfig: {
+            hide: {
+              animation: "spring",
+            },
+          },
+          tabBarHideOnKeyboard: true,
+        }}
+      />
+      <StatusBar style="dark" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        //   headerBackground: () => (
+        //     <View
+        //       style={{
+        //         backgroundColor: "red",
+        //         flexGrow: 1,
+        //       }}
+        //     />
+        //   ),
+    
+        }}
+      />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
